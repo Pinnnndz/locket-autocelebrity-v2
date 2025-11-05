7fe5c37570ad2924292c19a54b27479143770b39
Vo Huu Phuoc <118438452+Pinnnndz@users.noreply.github.com>
2025-11-05 16:04:14 +0700

    tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,1 +1,15 @@
-
+// ==UserScript==
+// @name         Auto Celebrity V2
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Nâng cấp UI Trạng thái
+// @author       Huu Phuoc
+// @require      https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-v2/main/auto-celeb.main.js
+// @homepageURL  https://github.com/Pinnnndz/locket-autocelebrity-v2
+// @downloadURL  https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-v2/main/tampermonkey.user.js
+// @updateURL    https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-v2/main/tampermonkey.user.js
+// @match        https://locket.binhake.dev/*
+// @run-at       document-idle
+// @grant        GM_addStyle
+// @icon         https://sf-static.upanhlaylink.com/img/image_202511051afc91d4c3c0b7839244cd213d339a23.jpg
+// ==/UserScript==
